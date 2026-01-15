1e9483586e42ecb52aafbff3fa1071f6c64fe117
Nirren404 <linajana290778@gmail.com>
2026-01-15 16:08:11 +0100

    doing service business logic excercise not done and created branch

---
+++ src/services/product.services.ts
@@ -0,0 +1,18 @@
+//Step 2 done
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const products = [
+  { id: 1, name: "Apple", price: 100 },
+  { id: 2, name: "Pineapple", price: 150 },
+];
+
+// Step 3  done,
+const findAll = () => {
+  return products;
+};
+
+const createProduct = () => {};
