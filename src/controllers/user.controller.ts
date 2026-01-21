4a4db8696f2131a19f1624045c9856474c4d58a7
Nirren404 <linajana290778@gmail.com>
2026-01-21 19:20:29 +0100

    feat: crud operations excercise not done checking problem

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,10 @@
 import type { Request, Response } from "express";
 import * as userService from "../services/user.service";
 
-export const createUser = async (req: Request, res: Response) => {
+export const createUser = async (
+  req: Request<{}, {}, userService.newUser>,
+  res: Response,
+) => {
   try {
     const { name, email, age } = req.body;
     const newUser = await userService.createUser(name, email, age);
