d4696b22ac27d97d04c94d23565954a3b995aaae
Nirren404 <linajana290778@gmail.com>
2026-01-21 19:15:38 +0100

    feat: crud operations excercise not done checking problem

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,5 +6,16 @@ export const createUser = async (req: Request, res: Response) => {
     const { name, email, age } = req.body;
     const newUser = await userService.createUser(name, email, age);
     res.status(201).json(newUser);
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ msg: "User creation failed", error });
+  }
+};
+
+export const getAllUser = async (req: Request, res: Response) => {
+  try {
+    const users = await userService.getAllUsers;
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ msg: "Failed to retrive users", error });
+  }
 };
