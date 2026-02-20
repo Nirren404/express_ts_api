b1692258e3af1166d69842aea4e41f218820db30
Nirren404 <linajana290778@gmail.com>
2026-02-20 09:45:58 +0100

    feat sql commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
 import { createApp } from "./app";
 import { dot } from "node:test/reporters";
+import { pool } from "./config/db";
 
 dotenv.config();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 const app = createApp();
+pool
+  .connect()
+  .then(() => console.log("DB connected ✅✅🫡"))
+  .catch((e) => console.log(e));
 
 app.listen(PORT, () => {
   console.log(` Server running on  http://localhost:${PORT} `);
